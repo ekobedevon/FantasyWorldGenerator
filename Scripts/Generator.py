cbf1a7ed5af2adb99f6056307ff419a41745c367
Kobe Edmond <e.kobedevon@gmail.com>
2021-12-25 16:40:44 -0600

    added in base professional read in from files. Both sorted by categories and non sorted

--- Scripts/Generator.py
+++ Scripts/Generator.py
@@ -1,21 +1,6 @@
 import json as js
 import os
-
-
-class generator(): # create a generator object
-     #Class Variables
-    race_list = {} # a dictionary of all the races
-    #create a generator object that stores all the data at the start
-    def __init__(self) -> None: 
-        os.chdir("Json_Files\Races")
-        race_fileList = os.listdir("./") # all races in the files
-        for race in race_fileList:
-            data = initialize_race(race) 
-            if type(data) != type(1): #if the data type is not an int
-                self.race_list[race] = data # store in data
-            else:
-                print(race + "is not in a valid format, checking settings file in race folder");
-
+import random as rand
 
 def initialize_race(race: str):
     """Used to get all the information for each race in the Json File folder for races"""
@@ -29,6 +14,67 @@ def initialize_race(race: str):
             race_dict[file_name.removesuffix('.json')] = js.load(file)
     if len(race_dict["Settings"]) != 2: #if settings doesn't have current minimum settings
         return -1
-        
     return race_dict
+
+def intialize_professions():
+    """Read in from the professions files and generate the lists needed for world generation"""
+    os.chdir("Json_Files\Professions") #set to profession directory
+    profession_fileList = os.listdir("./")
+    profession_master=[] #list of ALL professions
+    professions_Categorized = {} #professions sorted by categories
+    for file_name in profession_fileList:
+        file = open(file_name);
+        data = js.load(file) # load the data into an list
+        profession_master = profession_master + data # add the list to the master
+        professions_Categorized[file_name.removesuffix(".json")] = data #store in dicitonary
+
+    return profession_master,professions_Categorized
+
+class generator(): # create a generator object
+     #Class Variables
+    race_list = {} # a dictionary of all the races
+    profession_master=[] #master list of all jobs
+    professions_Categorized = {} #jobs sorted by categories
+    #create a generator object that stores all the data at the start
+    def __init__(self) -> None:
+        base = os.getcwd()
+        os.chdir("Json_Files\Races")
+        race_fileList = os.listdir("./") # all races in the files
+        for race in race_fileList:
+            data = initialize_race(race) 
+            if type(data) != type(1): #if the data type is not an int
+                self.race_list[race] = data # store in data
+            else:
+                print(race + "is not in a valid format, checking settings file in race folder")
+        os.chdir(base)
+        profession_master, professions_Categorized = intialize_professions() #intialize profession master
+        
+
+
+
+    def generateName(self,race: str, sex: str = ""):
+        """Given a string, generate a name based of settings"""
+        name = ""
+        match len(self.race_list[race]):
+            case 1:
+                print("Not enough arguments")
+            case 2:
+                print("Non gendered names to come")
+            case 3:# races with no last names 
+                if sex == "M" or sex == "F":
+                    name = rand.choice(self.race_list[race][race+"_"+sex])
+            case 4: #races with simple last names
+                if sex == "M" or sex == "F":
+                    name = rand.choice(self.race_list[race][race+"_"+sex])
+                name = name + " " + rand.choice(self.race_list[race][race+"_Last"])
+            case 5: #races with complex last names(2 parts)
+                if sex == "M" or sex == "F":
+                    name = rand.choice(self.race_list[race][race+"_"+sex])
+                name = name + " of " + rand.choice(self.race_list[race][race+"_Pre"])  + " " + rand.choice(self.race_list[race][race+"_Post"])
+        return name
+
+
+
+
+
 
